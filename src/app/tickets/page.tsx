1ea798e57c52eb2c846edebf1d4e13ed0a6885ed
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-07-17 18:50:54 +0400

    ✨ Add createTicket action and create ticket card with create form

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,5 +1,7 @@
 import { Heading } from "@/components/heading";
 import { Spinner } from "@/components/spinner";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { TicketCreateForm } from "@/features/ticket/components/ticket-create-form";
 import { TicketList } from "@/features/ticket/components/ticket-list";
 import { Suspense } from "react";
 
@@ -7,7 +9,24 @@ const TicketsPage =  () => {
   
   return (
     <div className="flex-1 flex flex-col gap-y-10">
-      <Heading title="TicketsPage" description="All your tickets. Zero hassle."/>      
+      <Heading title="Tickets" description="All your tickets. Zero hassle."/>      
+        
+        <Card className="w-full max-w-[420px] self-center">
+          <CardHeader>
+            <CardTitle>
+              Create Ticket
+            </CardTitle>
+            <CardDescription>
+            A new ticket will be created
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            < TicketCreateForm />
+          </CardContent>
+        </Card>
+
+
+        
         <Suspense fallback={<Spinner />}>
           <TicketList />
         </Suspense>
