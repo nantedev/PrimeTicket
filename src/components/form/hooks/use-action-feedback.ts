b18f70441476300f524c1f3543509af207c50a39
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-07-24 17:32:12 +0400

    🐛 Debugging custom useActionFeedback hook

--- src/components/form/hooks/use-action-feedback.ts
+++ src/components/form/hooks/use-action-feedback.ts
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect, useRef } from "react"
 import { ActionState } from "../utils/to-action-state"
 
 type OnArgs = {
@@ -14,7 +14,15 @@ const useActionFeedback = (
     actionState: ActionState,
     options: UseActionFeedbackOptions,
 ) => {
+
+    const prevTimestamp = useRef(actionState.timestamp);
+    const isUpdate = prevTimestamp.current !== actionState.timestamp;
+
+    
     useEffect(() => {
+
+    if(!isUpdate) return;
+
     if (actionState.status === "SUCCESS") {
             options.onSuccess?.({actionState});
     }
@@ -23,7 +31,7 @@ const useActionFeedback = (
             options.onError?.({actionState});
    }
 
-    }, [actionState, options]);
+    }, [isUpdate, actionState, options]);
 
 }
 
