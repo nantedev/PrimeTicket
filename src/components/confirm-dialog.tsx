41294d347d0076817dd3576b8c871faaf8367962
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-08-03 22:19:36 +0400

    🚧 confirm dialog on delete

---
+++ src/components/confirm-dialog.tsx
@@ -0,0 +1,49 @@
+import { ActionState } from "./form/utils/to-action-state";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "./ui/button";
+
+type ConfirmDialogProps = {
+  action: (payload: FormData) => void;
+  trigger: React.ReactNode;
+  title?: string;
+  description?: string;
+};
+
+const ConfirmDialog = ({
+  action,
+  trigger,
+  title = "Are you absolutely sure ?",
+  description = "This action cannot be undone. Make sur you understand the consequences.",
+}: ConfirmDialogProps) => {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{trigger}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction asChild>
+            <form action={action}>
+              <Button type="submit">Confirm</Button>
+            </form>
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export { ConfirmDialog };
