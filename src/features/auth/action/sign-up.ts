ccc9c586fc2e793ddbf557bfa78283510e60df02
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-08-07 00:07:33 +0400

    ✨ Set Sign Up

---
+++ src/features/auth/action/sign-up.ts
@@ -0,0 +1,54 @@
+"use server";
+
+import { ActionState, fromErrorToActionState } from "@/components/form/utils/to-action-state";
+import { lucia } from "@/lib/lucia";
+import { prisma } from "@/lib/prisma";
+import { cookies } from "next/headers";
+import { hash } from "@node-rs/argon2";
+import z from "zod"
+import { redirect } from "next/navigation";
+import { ticketsPath } from "@/paths";
+
+const signUpSchema = z.object({
+    username: z
+      .string()
+      .min(1)
+      .max(191)
+      .refine(
+        (value) => !value.includes(" "),
+        "Username cannot contain spaces"
+      ),
+    email: z.email().min(1, { message: "Is required" }).max(191),
+    password: z.string().min(6).max(191),
+    confirmPassword: z.string().min(6).max(191),
+  })
+    .refine((data) => data.password === data.confirmPassword, {
+      message: "Passwords do not match",
+      path: ["confirmPassword"],
+    });
+
+  export const signUp = async (_actionState: ActionState, formData: FormData) => {
+    try {
+        const { username, email, password } = signUpSchema.parse(
+        Object.fromEntries(formData)
+    )
+    const passwordHash = await hash(password);
+
+    const user = await prisma.user.create({
+        data: {
+            username,
+            email,
+            passwordHash
+        }
+    })
+
+    const session = await lucia.createSession(user.id, {});
+    const sessionCookie = lucia.createSessionCookie(session.id);
+
+    (await cookies()).set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+
+    } catch (error) {
+        return fromErrorToActionState(error, formData)
+    }
+    redirect(ticketsPath())
+  }
