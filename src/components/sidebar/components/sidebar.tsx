56b358ee78bc3e15f393b807e3d3680e8c10d677
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-08-20 15:44:09 +0400

    ✨ Authenticated Sidebar

--- src/components/sidebar/components/sidebar.tsx
+++ src/components/sidebar/components/sidebar.tsx
@@ -3,8 +3,10 @@ import { useState } from "react";
 import { navItems } from "../constant";
 import { cn } from "@/lib/utils";
 import { SidebarItem } from "./sidebar-item";
+import { useAuth } from "@/features/auth/hooks/use-auth";
 
 const Sidebar = () => {
+  const { user, isFetched } = useAuth();
   const [isOpen, setOpen] = useState(false);
   const [isTransition, setTransition] = useState(false);
 
@@ -16,9 +18,14 @@ const Sidebar = () => {
     }, 300);
   };
 
+  if (!user || !isFetched) {
+    return <div className="w-[78px] bg-secondary/20" />;
+  }
+
   return (
     <nav
       className={cn(
+        "animate-sidebar-from-left",
         "h-screen border-r pt-24",
         isTransition && "duration-200",
         isOpen ? "md:w-60 w-[78px]" : "w-[78px]"
