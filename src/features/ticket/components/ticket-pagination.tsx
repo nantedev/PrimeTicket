05e6f0139ea409b79c4fc3b4b99f1688a33ce4b7
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-09-14 11:46:38 +0400

    ⚡️ Resets pagination on search query change

--- src/features/ticket/components/ticket-pagination.tsx
+++ src/features/ticket/components/ticket-pagination.tsx
@@ -1,8 +1,13 @@
 "use client";
 
-import { useQueryStates } from "nuqs";
-import { paginationOptions, paginationParser } from "../search-params";
+import { useQueryState, useQueryStates } from "nuqs";
+import {
+  paginationOptions,
+  paginationParser,
+  searchParser,
+} from "../search-params";
 import { Pagination } from "@/components/pagination";
+import { useEffect, useRef } from "react";
 
 type TicketPaginationProps = {
   paginatedTicketMetadata: {
@@ -18,6 +23,15 @@ const TicketPagination = ({
     paginationParser,
     paginationOptions
   );
+
+  const [search] = useQueryState("search", searchParser);
+  const prevSearch = useRef(search);
+
+  useEffect(() => {
+    if (search === prevSearch.current) return;
+    prevSearch.current = search;
+    setPagination({ ...pagination, page: 0 });
+  }, [search, pagination, setPagination]);
 
   return (
     <Pagination
