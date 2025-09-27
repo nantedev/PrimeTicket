10da4867daf9d4151fa22a74638a40a90e1be7f9
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-09-27 10:51:08 +0400

    🐛 fix params type

--- src/app/api/tickets/[ticketId]/route.ts
+++ src/app/api/tickets/[ticketId]/route.ts
@@ -1,11 +1,11 @@
 import { getTicket } from "@/features/ticket/queries/get-ticket";
-import { NextResponse } from "next/server";
 
 export async function GET(
-    _request: Request,
-    { params }: { params: { ticketId: string } }
+  _request: Request,
+  { params }: { params: Promise<{ ticketId: string }> }
 ) {
-    const { ticketId } = params;
-    const ticket = await getTicket(ticketId);
-    return NextResponse.json(ticket);
+  const { ticketId } = await params;
+  const ticket = await getTicket(ticketId);
+
+  return Response.json(ticket);
 }
