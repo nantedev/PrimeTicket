6c746171e0c025735f2f52c60fdec63457a7dd69
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-09-30 14:54:22 +0400

    🚧  COMMENT > create comments

--- src/features/comment/components/comments.tsx
+++ src/features/comment/components/comments.tsx
@@ -1,5 +1,7 @@
+import { CardCompact } from "@/components/card-compact";
 import { getComments } from "../queries/get-comments";
 import { CommentItem } from "./comment-item";
+import { CommentCreateForm } from "./comment-create-form";
 
 type CommentsProps = {
   ticketId: string;
@@ -8,11 +10,19 @@ type CommentsProps = {
 const Comments = async ({ ticketId }: CommentsProps) => {
   const comments = await getComments(ticketId);
   return (
-    <div className="flex flex-col gap-y-2 ml-8">
-      {comments.map((comment) => (
-        <CommentItem key={comment.id} comment={comment} />
-      ))}
-    </div>
+    <>
+      <CardCompact
+        title="Created Comments"
+        description="A new comment will be created"
+        content={<CommentCreateForm ticketId={ticketId} />}
+      />
+
+      <div className="flex flex-col gap-y-2 ml-8">
+        {comments.map((comment) => (
+          <CommentItem key={comment.id} comment={comment} />
+        ))}
+      </div>
+    </>
   );
 };
 
