b51a63e531b951ccc3ef891e9ad441e163d98f18
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-09-30 18:11:18 +0400

    🚧  COMMENT > delete comments

--- src/features/comment/components/comments.tsx
+++ src/features/comment/components/comments.tsx
@@ -2,6 +2,9 @@ import { CardCompact } from "@/components/card-compact";
 import { getComments } from "../queries/get-comments";
 import { CommentItem } from "./comment-item";
 import { CommentCreateForm } from "./comment-create-form";
+import { isOwner } from "@/features/auth/utils/is-owner";
+import { getAuth } from "@/features/auth/queries/get-auth";
+import { CommentDeleteButton } from "./comment-delete-button";
 
 type CommentsProps = {
   ticketId: string;
@@ -9,6 +12,7 @@ type CommentsProps = {
 
 const Comments = async ({ ticketId }: CommentsProps) => {
   const comments = await getComments(ticketId);
+  const { user } = await getAuth();
   return (
     <>
       <CardCompact
@@ -19,7 +23,15 @@ const Comments = async ({ ticketId }: CommentsProps) => {
 
       <div className="flex flex-col gap-y-2 ml-8">
         {comments.map((comment) => (
-          <CommentItem key={comment.id} comment={comment} />
+          <CommentItem
+            key={comment.id}
+            comment={comment}
+            buttons={[
+              ...(isOwner(user, comment)
+                ? [<CommentDeleteButton key="0" id={comment.id} />]
+                : []),
+            ]}
+          />
         ))}
       </div>
     </>
